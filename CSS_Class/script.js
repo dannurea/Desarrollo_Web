29ae28c90ac6f2057102e51e43390dff20c21005
Daniela Rea <0234611@up.edu.mx>
2022-08-22 15:19:55 -0500

    CSS_Class_19_08_2022

---
+++ CSS_Class/script.js
@@ -0,0 +1,24 @@
+//Legado
+var var1= 'var1'
+
+//
+const constl='constl'
+// constl=14;
+
+let letl='letl';
+letl = 14
+
+console.log('lentl', letl)
+console.log('lentl')
+console.warn('constl,lentl', constl, letl)
+
+//Legado
+function name(params){
+
+}
+
+const myFunction = (txt) => {
+    console.log(txt, 'from func')
+}
+
+myFunction('Hola')
